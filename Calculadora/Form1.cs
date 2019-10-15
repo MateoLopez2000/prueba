eb973350ee24a5bb999277b6aa353dbd918b83a5
MateoLopez2000 <lm67427572@gmail.com>
2019-10-15 12:49:29 -0400

    prueba

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private double raiz (  long val)
+        {
+            double result2 = 0;
+            result2 = Math.Sqrt(val);
+            return result2;
+        }
+        private long suma(long a , long b )
+        {
+            long result = 0;
+            result = a + b;
+            return result;
+        }
+    }
+}
